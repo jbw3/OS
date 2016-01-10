e9250737b02eec565ecebb0bcc12196aac30a293
John Wilkes <jwilkes0011@gmail.com>
2016-01-09 22:29:54 -0600

    Adding functions to add a page table to the page dir and a page to a page table

--- paging.cpp
+++ paging.cpp
@@ -1,12 +1,23 @@
 #include "paging.h"
 #include "screen.h"
+#include "system.h"
 
-void initPaging()
+namespace
 {
-    installIrqHandler(IRQ_PAGE_FAULT, pageFault);
-    initPageDir();
-    initPageTable(0);
-    // enablePaging();
+
+/**
+ * @brief Identity map the kernel
+ */
+void mapKernel()
+{
+    uint32_t kStart = reinterpret_cast<uint32_t>(getKImgStart());
+    uint32_t kEnd = reinterpret_cast<uint32_t>(getKImgEnd());
+    for (uint32_t pageAddr = kStart; pageAddr < kEnd; pageAddr += 4096)
+    {
+        addPage(pageAddr);
+    }
+}
+
 }
 
 extern "C"
@@ -25,4 +36,61 @@ void pageFault(const registers* /*regs*/)
 
     // hang
     while (true);
+}
+
+void initPaging()
+{
+    // register page fault handler
+    installIrqHandler(IRQ_PAGE_FAULT, pageFault);
+
+    // initialize the page driectory
+    initPageDir();
+
+    // add a page table
+    initPageTable(0);
+    addPageTable(0);
+    addPage(0);
+
+    mapKernel();
+
+    // enablePaging();
+}
+
+void addPageTable(uint32_t pageTableAddr)
+{
+    uint32_t* pageDir = getPageDirStart();
+
+    // calculate the page directory index
+    int pageDirIdx = pageTableAddr >> 22;
+
+    // get the page directory entry from the page directory
+    uint32_t pageDirEntry = pageDir[pageDirIdx];
+
+    // add the page table address to the page directory
+    pageDirEntry &= ~PAGE_DIR_ADDRESS;
+    pageDirEntry |= (pageTableAddr & PAGE_DIR_ADDRESS);
+    pageDirEntry |= PAGE_DIR_PRESENT;
+    pageDir[pageDirIdx] = pageDirEntry;
+}
+
+void addPage(uint32_t pageAddr)
+{
+    uint32_t* pageDir = getPageDirStart();
+
+    // calculate the page directory and page table indexes
+    int pageDirIdx = pageAddr >> 22;
+    int pageTableIdx = (pageAddr >> 12) & 0x3FF;
+
+    // get the page table address from the page directory
+    uint32_t pageDirEntry = pageDir[pageDirIdx];
+    uint32_t* pageTable = reinterpret_cast<uint32_t*>(pageDirEntry & PAGE_DIR_ADDRESS);
+
+    // get the page address from the page table
+    uint32_t pageTableEntry = pageTable[pageTableIdx];
+
+    // add the page address to the page table
+    pageTableEntry &= ~PAGE_TABLE_ADDRESS;
+    pageTableEntry |= (pageAddr & PAGE_TABLE_ADDRESS);
+    pageTableEntry |= PAGE_TABLE_PRESENT;
+    pageTable[pageTableIdx] = pageTableEntry;
 }
--- paging.h
+++ paging.h
@@ -5,12 +5,18 @@
 
 #include "irq.h"
 
+#define PAGE_DIR_ADDRESS 0xFFFFF000
+#define PAGE_DIR_PRESENT 0x00000001
+
+#define PAGE_TABLE_ADDRESS 0xFFFFF000
+#define PAGE_TABLE_PRESENT 0x00000001
+
 extern "C"
 {
 
-const uint32_t* getPageDirStart();
+uint32_t* getPageDirStart();
 
-const uint32_t* getPageDirEnd();
+uint32_t* getPageDirEnd();
 
 /**
  * @brief Initialize the page directory
@@ -30,5 +36,15 @@ void pageFault(const registers* regs);
 } // extern "C"
 
 void initPaging();
+
+/**
+ * @brief Add a page table to the page directory
+ */
+void addPageTable(uint32_t pageTableAddr);
+
+/**
+ * @brief Add a page to a page table
+ */
+void addPage(uint32_t pageAddr);
 
 #endif // PAGING_H_
