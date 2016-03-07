867dd87a1fd1cc311cdb697cb3885300cb864be9
John Wilkes <jwilkes0011@gmail.com>
2016-03-06 21:43:37 -0600

    Adding braces around script vars

--- tools/createIso.sh
+++ tools/createIso.sh
@@ -8,7 +8,7 @@ fi
 
 ARCH_NAME=$1
 
-mkdir -p bin/isodir-$ARCH_NAME/boot/grub
-cp bin/kernel-$ARCH_NAME bin/isodir-$ARCH_NAME/boot/kernel
-cp grub.cfg bin/isodir-$ARCH_NAME/boot/grub/grub.cfg
-grub-mkrescue bin/isodir-$ARCH_NAME -o bin/SandboxOS-$ARCH_NAME.iso
+mkdir -p bin/isodir-${ARCH_NAME}/boot/grub
+cp bin/kernel-${ARCH_NAME} bin/isodir-${ARCH_NAME}/boot/kernel
+cp grub.cfg bin/isodir-${ARCH_NAME}/boot/grub/grub.cfg
+grub-mkrescue bin/isodir-${ARCH_NAME} -o bin/SandboxOS-${ARCH_NAME}.iso
