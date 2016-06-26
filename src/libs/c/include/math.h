0e5ffd2dfc188adf861d081c33b1c1c2129a26a0
John Wilkes <jwilkes0011@gmail.com>
2016-06-26 15:49:31 -0500

    Adding math.h

---
+++ src/libs/c/include/math.h
@@ -0,0 +1,23 @@
+#ifndef _MATH_H
+#define _MATH_H 1
+
+#define M_E        2.718281828459045235360287471353
+#define M_PI       3.141592653589793238462643383280
+#define M_PI_2     1.570796326794896619256404479703
+#define M_PI_4     0.785398163397448309628202239852
+#define M_1_PI     0.318309886183790671538174771316
+#define M_2_PI     0.636619772367581343076349542631
+#define M_2_SQRTPI 1.128379167095512573847949922001
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* _MATH_H */
