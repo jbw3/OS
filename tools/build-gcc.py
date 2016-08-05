1c2e60171f604c24e464a47490983590be0b0ac1
John Wilkes <jwilkes0011@gmail.com>
2016-08-04 20:31:53 -0500

    Python script now builds GCC

--- tools/build-gcc.py
+++ tools/build-gcc.py
@@ -29,7 +29,6 @@ class Builder(object):
                      '--with-sysroot',
                      '--disable-nls',
                      '--disable-werror']
-        print(' '.join(configCmd))
         proc = subprocess.Popen(configCmd, cwd=buildPath)
         proc.wait()
 
@@ -44,8 +43,48 @@ class Builder(object):
         # delete build directory
         shutil.rmtree(buildPath)
 
+    def _buildGcc(self):
+        buildPath = self._makeTempDir('build-gcc')
+
+        # set environment vars
+        binutilsBinDir = os.path.join(self.args.output, 'bin')
+        env = os.environ
+        env['PATH'] = binutilsBinDir + ':' + env['PATH']
+
+        # get dependencies
+        proc = subprocess.Popen(['./contrib/download_prerequisites'], cwd=self.args.gccSrc)
+        proc.wait()
+
+        # configure
+        configCmd = [os.path.join(self.args.gccSrc, 'configure'),
+                     '--target={}'.format(self.args.target),
+                     '--prefix={}'.format(self.args.output),
+                     '--disable-nls',
+                     '--enable-languages=c,c++',
+                     '--without-headers']
+        proc = subprocess.Popen(configCmd, cwd=buildPath, env=env)
+        proc.wait()
+
+        # build
+        proc = subprocess.Popen(['make', 'all-gcc'], cwd=buildPath, env=env)
+        proc.wait()
+
+        proc = subprocess.Popen(['make', 'all-target-libgcc'], cwd=buildPath, env=env)
+        proc.wait()
+
+        # install
+        proc = subprocess.Popen(['make', 'install-gcc'], cwd=buildPath, env=env)
+        proc.wait()
+
+        proc = subprocess.Popen(['make', 'install-target-libgcc'], cwd=buildPath, env=env)
+        proc.wait()
+
+        # delete build directory
+        shutil.rmtree(buildPath)
+
     def build(self):
         self._buildBinutils()
+        self._buildGcc()
 
 def parseArgs():
     import argparse
