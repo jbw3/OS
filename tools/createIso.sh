8df25bd191b122408800df0c1d797d28990634f9
John Wilkes <jwilkes0011@gmail.com>
2017-06-04 21:13:15 -0500

    Adding modules to ISO

--- tools/createIso.sh
+++ tools/createIso.sh
@@ -13,8 +13,21 @@ DIR_NAME=$(dirname ${FULL_NAME})
 TOP_DIR=${DIR_NAME}/..
 BIN_DIR=${TOP_DIR}/bin
 ARCH_NAME=$1
+ISO_DIR=${BIN_DIR}/isodir-${ARCH_NAME}
 
-mkdir -p ${BIN_DIR}/isodir-${ARCH_NAME}/boot/grub
-cp ${BIN_DIR}/kernel-${ARCH_NAME} ${BIN_DIR}/isodir-${ARCH_NAME}/boot/kernel
-cp ${TOP_DIR}/grub.cfg ${BIN_DIR}/isodir-${ARCH_NAME}/boot/grub/grub.cfg
-grub-mkrescue /usr/lib/grub/i386-pc ${BIN_DIR}/isodir-${ARCH_NAME} -o ${BIN_DIR}/SandboxOS-${ARCH_NAME}.iso
+# create directories
+mkdir -p ${ISO_DIR}/boot/grub
+mkdir -p ${ISO_DIR}/modules
+
+# copy kernel
+cp ${BIN_DIR}/kernel-${ARCH_NAME} ${ISO_DIR}/boot/kernel
+
+# copy GRUB config file
+cp ${TOP_DIR}/grub.cfg ${ISO_DIR}/boot/grub/grub.cfg
+
+# copy modules
+cp ${TOP_DIR}/bin/test1 ${ISO_DIR}/modules
+cp ${TOP_DIR}/bin/test2 ${ISO_DIR}/modules
+
+# make grub image
+grub-mkrescue /usr/lib/grub/i386-pc ${ISO_DIR} -o ${BIN_DIR}/SandboxOS-${ARCH_NAME}.iso
