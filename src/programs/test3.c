a4a3adf31a164e301b02d10d29ad7b6052ac5357
John Wilkes <jwilkes0011@gmail.com>
2017-06-18 16:17:20 -0500

    Updating test program to test system calls

--- src/programs/test3.c
+++ src/programs/test3.c
@@ -1,4 +1,15 @@
+#include "stddef.h"
+#include "string.h"
+#include "unistd.h"
+
 int main()
 {
-    return 3;
+    const char str[] = "Hi there!\n";
+
+    for (int i = 0; i < 2; ++i)
+    {
+        write(STDOUT_FILENO, str, strlen(str));
+    }
+
+    return 0;
 }
