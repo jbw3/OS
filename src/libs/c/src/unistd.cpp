d390f2db7b1a91a0a8b1809406c2f8285c69155b
John Wilkes <jwilkes0011@gmail.com>
2017-06-18 23:15:08 -0500

    Calling write() system call

--- src/libs/c/src/unistd.cpp
+++ src/libs/c/src/unistd.cpp
@@ -6,14 +6,12 @@ extern "C"
 
 ssize_t write(int fildes, const void* buf, size_t nbyte)
 {
-    /// @todo implement
+    systemCall3(SYSTEM_CALL_WRITE,
+                fildes,
+                reinterpret_cast<uint32_t>(buf),
+                nbyte);
 
-    // systemCall3(SYSTEM_CALL_WRITE,
-    //             fildes,
-    //             reinterpret_cast<uint32_t>(buf),
-    //             nbyte);
-    systemCall3(10, 10, 52, 12345);
-
+    /// @todo return code from system call
     return -1;
 }
 
