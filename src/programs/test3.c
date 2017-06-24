81d69ca7d7e3e9bddac51f6526a56147f7e9b64e
John Wilkes <jwilkes0011@gmail.com>
2017-06-24 14:25:25 -0500

    Updating test program

--- src/programs/test3.c
+++ src/programs/test3.c
@@ -1,15 +1,34 @@
 #include "stddef.h"
+#include "stdio.h"
 #include "string.h"
 #include "unistd.h"
+
+void print(const char* str)
+{
+    write(STDOUT_FILENO, str, strlen(str));
+}
 
 int main()
 {
     const char str[] = "Hi there!\n";
+    int rc = 123456;
 
-    for (int i = 0; i < 2; ++i)
+    rc = write(STDOUT_FILENO, str, strlen(str));
+    if (rc == 0)
     {
-        write(STDOUT_FILENO, str, strlen(str));
+        print("rc = 0\n");
     }
+
+    rc = write(STDERR_FILENO, str, strlen(str));
+    if (rc == -1)
+    {
+        print("rc = -1\n");
+    }
+
+    pid_t pid = getpid();
+    char pidBuff[32];
+    sprintf(pidBuff, "pid: %i\n", pid);
+    print(pidBuff);
 
     return 0;
 }
