6371dc51b3385f5416d0b86a1e6b4ca4481229da
lasserre <cstew33@gmail.com>
2017-06-30 05:53:27 -0500

    Modified run script to actually use hard drive via AHCI

--- runAhciDrive.sh
+++ runAhciDrive.sh
@@ -1,7 +1,8 @@
-qemu-system-i386 -kernel bin/kernel-x86 -monitor stdio -M q35
+qemu-system-i386 -kernel bin/kernel-x86 -monitor stdio -M q35 \
+-hda osDrive.img
 
-#-drive file=osDrive.img,if=none,id=myDrive \
-# I don't think I need the ich9-ahci device with -M q35 option b/c it
-# ------------
-# appears to be built in...
-#-device ich9-ahci,id=ahci -device ide-drive,drive=myDrive,bus=ahci.0
+# or try this...
+# -----------------
+# -drive file=osDrive.img,if=none,id=myDrive \
+# -device ich9-ahci,id=ahci \
+# -device ide-drive,drive=myDrive,bus=ahci.0
