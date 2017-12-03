480e62237d52f0ecfee40f45df7dd8b7af91ed67
John Wilkes <jwilkes0011@gmail.com>
2017-12-03 17:57:15 -0600

    First cut at unmap

--- src/kernel/arch/x86/32Bit/pagetreex86_32.cpp
+++ src/kernel/arch/x86/32Bit/pagetreex86_32.cpp
@@ -59,4 +59,26 @@ bool PageTreeX86_32::mapOnOrBefore(uintptr_t startAddr, uintptr_t& virtualAddr, 
 
 void PageTreeX86_32::unmap(uintptr_t virtualAddr)
 {
+    // calculate the page dir index
+    int pageDirIdx = virtualAddr & PAGE_DIR_INDEX_MASK;
+
+    // get the page dir entry
+    uint32_t pageDirEntry = pageDir[pageDirIdx];
+
+    // make sure the page table is present
+    if ( (pageDirEntry & PAGE_DIR_PRESENT) != 0 )
+    {
+        // get the page table
+        uint32_t pageTableAddr = pageDirEntry & PAGE_DIR_ADDRESS;
+        uint32_t* pageTable = reinterpret_cast<uint32_t*>(pageTableAddr);
+
+        // calculate the page table index
+        int pageTableIdx = (virtualAddr >> PAGE_TABLE_INDEX_SHIFT) & PAGE_TABLE_INDEX_MASK;
+
+        // clear the page table entry
+        pageTable[pageTableIdx] = 0;
+
+        // invalidate page in TLB
+        invalidatePage(virtualAddr);
+    }
 }
