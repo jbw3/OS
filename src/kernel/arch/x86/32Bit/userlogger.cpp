195e2e3ff7f036150cf93085fd65d6afd502af1b
John Wilkes <jwilkes0011@gmail.com>
2018-04-08 16:47:30 -0500

    Adding UserLogger

---
+++ src/kernel/arch/x86/32Bit/userlogger.cpp
@@ -0,0 +1,30 @@
+#include <stdint.h>
+
+#include "stream.h"
+#include "userlogger.h"
+
+UserLogger::UserLogger()
+{
+    streamsSize = 0;
+}
+
+void UserLogger::addStream(Stream* stream)
+{
+    if (streamsSize < MAX_STREAMS_SIZE)
+    {
+        streams[streamsSize++] = stream;
+    }
+}
+
+void UserLogger::flush(const char* buff, size_t len)
+{
+    const uint8_t* ptr = reinterpret_cast<const uint8_t*>(buff);
+
+    for (size_t i = 0; i < streamsSize; ++i)
+    {
+        streams[i]->write(ptr, len, /*block=*/ true);
+    }
+}
+
+// create user logger instance
+UserLogger ulog;
