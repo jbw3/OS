e3857a80b8526f953863f6f9b66a388c029385a7
John Wilkes <jwilkes0011@gmail.com>
2018-05-25 22:45:18 -0500

    Adding ASSERT_TRUE and ASSERT_FALSE

--- src/kernel/arch/x86/32Bit/test.h
+++ src/kernel/arch/x86/32Bit/test.h
@@ -10,6 +10,22 @@ do                               \
     return;                      \
 } while(false)
 
+#define ASSERT_BASE(evalFunc, msg)   \
+do                                   \
+{                                    \
+    if (!(evalFunc))                 \
+    {                                \
+        TestPrinter::printFail(msg); \
+        return;                      \
+    }                                \
+} while (false)
+
+#define ASSERT_TRUE(a) \
+ASSERT_BASE((a), #a" is false")
+
+#define ASSERT_FALSE(a) \
+ASSERT_BASE(!(a), #a" is true")
+
 void runTest(const char* name, void (*test)());
 
 #endif // TEST_H_
