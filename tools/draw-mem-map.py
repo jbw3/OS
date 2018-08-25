fbef29fe53c3ab93d115f7e7d2b84319b1253c1c
John Wilkes <jwilkes0011@gmail.com>
2018-08-25 14:13:58 -0500

    Drawing page dir

--- tools/draw-mem-map.py
+++ tools/draw-mem-map.py
@@ -2,8 +2,18 @@
 
 import plotly.offline as py
 import plotly.graph_objs as go
+import re
 
-def main():
+def parseArgs():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', default='kernel-x86.log', help='the input kernel log file')
+
+    args = parser.parse_args()
+    return args
+
+def draw(args):
     trace0 = go.Scatter(
         x=[2],
         y=[1],
@@ -22,21 +32,37 @@ def main():
             'range': [0, 2**32 - 1],
             'dtick': 2**30,
             'ticktext': ['A', 'B']
-            # 'tickvals': [i for i in range(2**32)],
         },
-        'shapes': [
-            {
-                'type': 'rect',
-                'x0': 0,
-                'x1': 4,
-                'y0': 0,
-                'y1': 2**20,
-            }
-        ]
+        'shapes': []
     }
+
+    pageSize = 4096
+    pageDir = None
+    foundPageDir = False
+    with open(args.input, 'r') as inFile:
+        for line in inFile:
+            if foundPageDir:
+                if line == 'DEBUG: Paging: PageDirEnd\n':
+                    break
+            elif line.startswith('DEBUG: Paging: PageDirStart: '):
+                foundPageDir = True
+                match = re.search(r'^DEBUG: Paging: PageDirStart: ([\da-fA-F]+)$', line)
+                pageDir = int(match.group(1), base=16)
+
+    layout['shapes'].append({
+        'type': 'rect',
+        'x0': 0,
+        'x1': 4,
+        'y0': pageDir,
+        'y1': pageDir + pageSize - 1
+    })
 
     fig = {'data': data, 'layout': layout}
     py.plot(fig, filename='test.html', auto_open=True)
+
+def main():
+    args = parseArgs()
+    draw(args)
 
 if __name__ == '__main__':
     main()
