7bbf0cbf8616cbb460e40d5b657f77928679567b
John Wilkes <jwilkes0011@gmail.com>
2018-08-25 14:40:00 -0500

    Drawing page tables

--- tools/draw-mem-map.py
+++ tools/draw-mem-map.py
@@ -4,6 +4,10 @@ import plotly.offline as py
 import plotly.graph_objs as go
 import re
 
+PAGE_DIR_COLOR = 'rgb(200, 0, 0)'
+PAGE_TABLE_COLOR = 'rgb(0, 10, 200)'
+PAGE_COLOR = 'rgb(0, 0, 0)'
+
 def parseArgs():
     import argparse
 
@@ -38,24 +42,49 @@ def draw(args):
 
     pageSize = 4096
     pageDir = None
-    foundPageDir = False
+    pageTables = []
+    foundPageInfo = False
     with open(args.input, 'r') as inFile:
         for line in inFile:
-            if foundPageDir:
+            if foundPageInfo:
                 if line == 'DEBUG: Paging: PageDirEnd\n':
                     break
-            elif line.startswith('DEBUG: Paging: PageDirStart: '):
-                foundPageDir = True
-                match = re.search(r'^DEBUG: Paging: PageDirStart: ([\da-fA-F]+)$', line)
-                pageDir = int(match.group(1), base=16)
+                else:
+                    match = re.search(r'^DEBUG: Paging: (.*): ([\da-fA-F]+)$', line)
+                    if match is not None:
+                        if match.group(1) == 'PageTable':
+                            pageTables.append(int(match.group(2), base=16))
+                        elif match.group(1) == 'PageDir':
+                            pageDir = int(match.group(2), base=16)
+            elif line == 'DEBUG: Paging: PageInfoStart\n':
+                foundPageInfo = True
 
+    # add page dir
     layout['shapes'].append({
         'type': 'rect',
         'x0': 0,
         'x1': 4,
         'y0': pageDir,
-        'y1': pageDir + pageSize - 1
+        'y1': pageDir + pageSize - 1,
+        'line': {
+            'color': PAGE_DIR_COLOR
+        },
+        'fillcolor': PAGE_DIR_COLOR
     })
+
+    # add page tables
+    for pageTable in pageTables:
+        layout['shapes'].append({
+            'type': 'rect',
+            'x0': 0,
+            'x1': 4,
+            'y0': pageTable,
+            'y1': pageTable + pageSize - 1,
+            'line': {
+                'color': PAGE_TABLE_COLOR
+            },
+            'fillcolor': PAGE_TABLE_COLOR
+        })
 
     fig = {'data': data, 'layout': layout}
     py.plot(fig, filename='test.html', auto_open=True)
